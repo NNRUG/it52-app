db312c48dc54c221999ec2975a6d8d52e4313bed
Anton Shrsmko <ashramko@mera.ru>
2016-08-06 17:38:40 +0300

    add material and vector icons lib

--- android/app/src/main/java/com/it52app/MainActivity.java
+++ android/app/src/main/java/com/it52app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.it52app;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 
 public class MainActivity extends ReactActivity {
 
